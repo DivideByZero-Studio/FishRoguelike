d1bc546a892bd2e0e9a5ebba57f0ff2a46c871d4
TWTwoFace <pipo2017@mail.ru>
2024-04-03 23:27:09 +0300

    fetch: Added spawn trigger

---
+++ Assets/Scripts/Spawner/SpawnTrigger.cs
@@ -0,0 +1,17 @@
+using System;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class SpawnTrigger : MonoBehaviour
+{
+    public event Action<Transform> OnEntered;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<PlayerStateMachine>(out var playerSM))
+        {
+            OnEntered?.Invoke(collision.transform);
+            Destroy(gameObject);
+        }
+    }
+}
