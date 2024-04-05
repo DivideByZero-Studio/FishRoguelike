fd01e342f845fda3be0c892c938e1c71d3b6ba0f
weeqn <gugobute@gmail.com>
2024-04-05 03:26:51 +0300

    fix: return missing files

---
+++ Assets/Scripts/Input/PlayerController.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private PlayerInput _input;
+    public event Action OnPrimaryAttack;
+    public event Action OnSecondaryAttack;
+    public event Action OnDash;
+
+    private void Awake()
+    {
+        _input = new PlayerInput();
+        _input.Player.Enable();
+    }
+
+    public Vector2 GetMovementNormalizedVector()
+    {
+        Vector2 direction = _input.Player.Move.ReadValue<Vector2>();
+        return direction;
+    }
+
+    private void Primary_attack_performed(InputAction.CallbackContext context)
+    {
+        OnPrimaryAttack?.Invoke();
+    }
+
+    private void Secondary_attack_performed(InputAction.CallbackContext context)
+    {
+        OnSecondaryAttack?.Invoke();
+    }
+
+    private void Dash_performed(InputAction.CallbackContext context)
+    {
+        OnDash?.Invoke();
+    }
+
+    private void OnEnable()
+    {
+        _input.Player.PrimaryAttack.performed += Primary_attack_performed;
+        _input.Player.SecondaryAttack.performed += Secondary_attack_performed;
+        _input.Player.Dash.performed += Dash_performed;
+    }
+
+    private void OnDisable()
+    {
+        _input.Player.PrimaryAttack.performed -= Primary_attack_performed;
+        _input.Player.SecondaryAttack.performed -= Secondary_attack_performed;
+        _input.Player.Dash.performed -= Dash_performed;
+    }
+}
